ccc630aec92c385ca45881e4d32d109ee20f2344
alexei002 <alexei002@gmail.com>
2021-01-25 07:11:39 +0300

    db for xampp

--- config/db.php
+++ config/db.php
@@ -2,9 +2,12 @@
 
 return [
     'class' => 'yii\db\Connection',
-	'dsn' => 'mysql:host=db;dbname=warstory_alexei',
-    'username' => 'root',
-    'password' => 'example',
+    //'dsn' => 'mysql:host=localhost;dbname=warstory',
+    'dsn' => 'mysql:host=localhost;dbname=warstory_alexei',
+    'username' => 'warstory_alexei',
+    //'username' => 'root',
+    'password' => 'ut9fpj34',
+    //'password' => '',
     'charset' => 'utf8',
     'tablePrefix' => 'au_',
 ];
